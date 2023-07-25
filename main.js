4c6d2be5d6d01b3a8d30f6b62436fa38fee657d9
Stewart Wingfield <sww915@gmail.com>
2023-07-24 22:06:24 -0400

    all tests pass

--- main.js
+++ main.js
@@ -9,10 +9,48 @@ const jobTypes = {
   programmer: 'Any Ship!'
 };
 
-// Your code will go here
+class CrewMember {
+  constructor(name, job, specialSkill, ship){
+  this.name = name 
+  this.job = job
+  this.specialSkill = specialSkill
+  this.ship = null
+  }
+  enterShip(ship) {
+    this.ship = ship;
+    ship.crew.push(this);
+    }
+  }
+console.log(new CrewMember)
+
+
+class Ship {
+  constructor(name, type, ability){
+  this.name = name
+  this.type = type
+  this.ability = ability
+  this.crew = []
+  }
+  missionStatement() {
+    if (this.crew.length === 0) {
+      return "Can't perform a mission yet."
+    }else {
+      return this.ability
+    }
+  }
+}
 
 
 
+
+
+
+
+
+let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit');
+const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+let hermes = new Ship('Hermes', 'Main Ship', 'Interplanetary Space Travel');
+const crewMember2 = new CrewMember('Commander Lewis', 'commander', 'geology')
 
 
 
